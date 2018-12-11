6cec88e9c47355f77b99cdafe3c602518cd00ebb
Isaac Vandor <isaacvandor@gmail.com>
2018-12-11 10:38:32 -0500

    Making more progress on looping issues

--- src/scripts/astar.py
+++ src/scripts/astar.py
@@ -8,75 +8,84 @@ import time
 import pdb
 
 class astar():
-    def actualAStar(self,start,finish,map):
+    def run_astar(self,init_pixel,dest_pixel,map):
         """
         Inputs: starting pixel, finishing pixel, map location
-        Outputs: Draws a line of the path the robot needs to take to get to the goalReached.
-        currently is still being optimized. It doesn't work excellently as it searches too much.
+        Outputs: Draws a line of the path the robot needs to take to get to the defined goal
         """
-        sizeOfRobot = 2 # determines the radius of the robot in pixels
+        robot_dims = 2 # determines the radius of the robot in pixels
         map = Image.open(map) # gets the map image
-        x_size = map.size[0] # the x length of the image
+        
+        # Get dimensions of the map
+        x_size = map.size[0]
         #print('x_size:', x_size)
-        y_size = map.size[1] # the y length of the image
+        y_size = map.size[1]
         #print('y_size:', y_size)
-        initialNumber = 10000 # sets the initial values for the Gs, Fs and Hs
+
+        #Set initial A* variable values
         Gs = {} # uses a dictionary to speed up the process
         Hs = {}
         Fs = {}
-        obstacles = {}
-        for i in range(x_size): # initializes the dictionary of obstacles based on the map.
+
+        obstacles = {} # initialize dict for obstacles found on map
+
+        # Params for filling obstacle dict
+        for i in range(x_size):
             for j in range(y_size):
                 if(map.getpixel((i,j))[1] < 200):
                     obstacles[(i,j)] = True
-        Gs[start] = 0
-        pastCoord = start # initializes the starting coordinate
-        #print('past coord:', pastCoord)
+        Gs[init_pixel] = 0
+        old_coord = init_pixel # initializes the starting coordinate
+        #print('start coord:', old_coord)
         #print('obstacles:', obstacles)
-        if (start in obstacles):
-            raise ValueError('Starting pixel is an obstacle; it cant be a starting point.')
-        if(finish in obstacles):
-            raise ValueError('Ending pixel is an obstacle; it cant be an ending point.')
+
+        # Check threshold is correct for obstacle detection
+        if (init_pixel in obstacles):
+            raise ValueError('initial pixel is an obstacle')
+        if(dest_pixel in obstacles):
+            raise ValueError('Ending pixel is an obstacle')
         openList = {}
-        closedList = {start:True}
+        closedList = {init_pixel:True}
         goalReached = False # a flag to check whether the goal has been reached yet.
+
+        # Run these jewels
         while(not goalReached):
-            adjacentCoords = [(pastCoord[0]+1,pastCoord[1]),(pastCoord[0]-1,pastCoord[1]),(pastCoord[0],pastCoord[1]+1),(pastCoord[0],pastCoord[1]-1)] # define the adjacentCoords to check
+            adjacentCoords = [(old_coord[0]+1,old_coord[1]),(old_coord[0]-1,old_coord[1]),(old_coord[0],old_coord[1]+1),(old_coord[0],old_coord[1]-1)] # define the adjacentCoords to check
             for i in adjacentCoords:
                 if(not i in closedList):
                     if(i[0] > 0 and i[0] < x_size and i[1] > 0 and i[1] < y_size): # making sure that the bounds aren't out of range.
-                        available = self.checkForBorders(i,sizeOfRobot,obstacles)
+                        available = self.self_check(i,robot_dims,obstacles)
                         if(available): # if the pixel is allowed, change it's cost
-                            Gs[i] = Gs[pastCoord] + 1 # changes the cost
-                            Hs[i] = abs(finish[1] - i[1]) + abs(finish[0] - i[0]) # change the h value to be the euclidean distance between the point and the finish
+                            Gs[i] = Gs[old_coord] + 1 # changes the cost
+                            Hs[i] = abs(dest_pixel[1] - i[1]) + abs(dest_pixel[0] - i[0]) # change the h value to be the euclidean distance between the point and the finish
                             Fs[i] = Hs[i] + Gs[i] # the F cost as the sum of the past two
                             openList[i] = Fs[i] # the open list contains the F value to make it easier to pick the lowest f value
                         else:
                             closedList[i] = True # If the pixel is not allowed, it is added to the closed list to prevent further checking.
-            if(finish in openList):
+            if(dest_pixel in openList):
                 goalReached = True
             min_val = min(openList.itervalues()) # gets the lowest F value in the dictionary of open pixels
             goodKeys = [k for k, v in openList.iteritems() if v == min_val] # gets the pixel(s) with the lowest F value
             HDict = {}
             for i in goodKeys:
                 HDict[i] = Hs[i]
-            pastCoord = min(HDict, key=HDict.get) # from here, gets the pixel with the lowest H value (most likely to be close to the goal)
-            openList.pop(pastCoord)
-            closedList[pastCoord] = True
-        ListOfCoordinates = self.plotPath(start,finish,Gs,map)
+            old_coord = min(HDict, key=HDict.get) # from here, gets the pixel with the lowest H value (most likely to be close to the goal)
+            openList.pop(old_coord)
+            closedList[old_coord] = True
+        ListOfCoordinates = self.plot_path(init_pixel,dest_pixel,Gs,map)
         #print('coord list:',ListOfCoordinates)
         return ListOfCoordinates
 
-    def plotPath(self,start,finish,Gs,map):
+    def plot_path(self,init_pixel,dest_pixel,Gs,map):
         solution = map
-        oldPixel = finish
-        ListOfCoordinates = [finish]
-        coords = [finish]
-        while(start != oldPixel): # while we havn't gotten to the start yet
+        oldPixel = dest_pixel
+        ListOfCoordinates = [dest_pixel]
+        coords = [dest_pixel]
+        while(init_pixel != oldPixel): # while we haven't gotten to the init_pixel yet
             coords = [(oldPixel[0]+1,oldPixel[1]),(oldPixel[0]-1,oldPixel[1]),(oldPixel[0],oldPixel[1]+1),(oldPixel[0],oldPixel[1]-1)] # checks the 4 surounding pixels
             Gvalues = [10000,10000,10000,10000] # initializes their values to 0
             for i in range(len(coords)): # puts their values in
-                try: # I added a try here bocause it sometimes goes out of bounds, and going out of bounds just means we can't go to that pixel
+                try: # has a tendency to go out of bounds (it can't get to pixel)
                     Gvalues[i] = Gs[coords[i]]
                 except:
                     Gvalues[i] = 10000
@@ -84,29 +93,29 @@ class astar():
             ListOfCoordinates = [coordToGoTo] + ListOfCoordinates
             solution.putpixel(coordToGoTo,(0,255,0)) # makes that coordinate red
             oldPixel = coordToGoTo # does it again
-        solution.save("../maps/processed/solution_goodmap.png")
+        solution.save("../maps/processed/solution_goodmap.png") # TODO: ideally this wouldnt be hardcoded
         #print('coord list:', ListOfCoordinates)
         return ListOfCoordinates
 
-    def checkForBorders(self,i,sizeOfRobot,obstacles):
+    def self_check(self,i,robot_dims,obstacles):
         """
         Checks the edges of the robot to determine if the pixel in question is a pixel that is allowed to be travelled
-        input: i, the coordinate in question, sizeOfRobot, the size of the robot in pixels, obstacles, the coordinates of all the obstacles
+        input: i, the coordinate in question, robot_dims, the size of the robot in pixels, obstacles, the coordinates of all the obstacles
         output: boolean, whether the pixel is available or not.
         """
-        for j in range(sizeOfRobot): # run through the edges of the square around the robot
+        for j in range(robot_dims): # run through the edges of the square around the robot
             k = 0 # top edge
-            if (i[0]+sizeOfRobot/2-j,i[1]+sizeOfRobot/2-k) in obstacles: # if a pixel on the edge is in the obstacle dictionary
+            if (i[0]+robot_dims/2-j,i[1]+robot_dims/2-k) in obstacles: # if a pixel on the edge is in the obstacle dictionary
                 return False # return False as the pixel is no longer allowed
-            k= sizeOfRobot # repeat for bottom edge
-            if(i[0]+sizeOfRobot/2-j,i[1]+sizeOfRobot/2-k) in obstacles:
+            k= robot_dims # repeat for bottom edge
+            if(i[0]+robot_dims/2-j,i[1]+robot_dims/2-k) in obstacles:
                 return False
-        for k in range(sizeOfRobot): # doing the same as above, but for the right and left edges
+        for k in range(robot_dims): # doing the same as above, but for the right and left edges
             j = 0
-            if(i[0]+sizeOfRobot/2-j,i[1]+sizeOfRobot/2-k) in obstacles:
+            if(i[0]+robot_dims/2-j,i[1]+robot_dims/2-k) in obstacles:
                 return False
-            j = sizeOfRobot
-            if(i[0]+sizeOfRobot/2-j,i[1]+sizeOfRobot/2-k) in obstacles:
+            j = robot_dims
+            if(i[0]+robot_dims/2-j,i[1]+robot_dims/2-k) in obstacles:
                 return False
         return True
 
@@ -117,7 +126,8 @@ if __name__ == '__main__':
     #init_y_coord = int(raw_input('init y coord: '))
     #dest_x_coord = int(raw_input('dest x coord: '))
     #dest_y_coord = int(raw_input('dest y coord: '))
-    #coordinates = nav.actualAStar((init_x_coord, init_y_coord), (dest_x_coord, dest_y_coord), "../maps/ac109_goodmap.png")
-    #coordinates = nav.actualAStar((580,325),(585,190),"../maps/ac109_goodmap.png") #Coords for corner of map to round trashcan
-    coordinates = nav.actualAStar((585,190),(665,225),"../maps/ac109_goodmap.png") #Coords for round trashcan to corner of box
-    print("--- %s seconds ---" % (time.time() - start_time))
+    #coordinates = nav.run_astar((init_x_coord, init_y_coord), (dest_x_coord, dest_y_coord), "../maps/ac109_goodmap.png")
+    #coordinates = nav.run_astar((580,325),(585,190),"../maps/ac109_goodmap.png") #Coords for corner of map to round trashcan
+    coordinates = nav.run_astar((585,190),(665,225),"../maps/ac109_goodmap.png") #Coords for round trashcan to corner of box
+    run_time = (time.time()-start_time)
+    print("--- %s seconds ---" % run_time)
--- src/scripts/navstar.py
+++ src/scripts/navstar.py
@@ -14,16 +14,17 @@ class navstar:
         self.pub = rospy.Publisher('/cmd_vel', Twist, queue_size=5)
         self.path = pixel
         self.time_step = time_step
-        #self.converting_factor = 0.0002645833
         self.converting_factor = 0.05
         self.linear_velocity = 0.0
         self.angular_velocity = 0.0
-        self.linear_vector = Vector3(x=0.0,y=0.0,z=0.0)
-        self.angular_vector = Vector3(x=0.0,y=0.0,z=0.0)
+        self.lin_vec = 0.0
+        self.ang_vec = 0.0
         self.old_direction = (1,0)
-        self.commands = []
         self.rate = rospy.Rate(5)
         self.commands = []
+        self.vel_msg = Twist()
+        self.distances = {}
+        self.directions = {}
 
     def linear_approximation(self, num):
         """
@@ -46,30 +47,27 @@ class navstar:
         :return: return distance and direction of each vector command
         """
         headings = self.linear_approximation(num)
-        distances = {}
-        directions = {}
         for i in range(len(headings)):
             x = headings[i][0]
             y = headings[i][1]
 
             # get distance and direction
             distance = math.sqrt(x ** 2 + y ** 2)
-            distances[i] = distance
+            self.distances[i] = distance
             direction = (1 / distance * x, 1 / distance * y)
-            directions[i] = direction
-        #print('dist, dir:', distances, directions)
-        return distances, directions
+            self.directions[i] = direction
+        print('dist, dir:', self.distances, self.directions)
+        return self.distances, self.directions
 
     def get_velocity_commands(self, num):
         """
         :param num: number of pixels skipped
         :return: return a list of commands with angular and linear velocity
         """
-        distances, directions = self.get_distance_direction(num)
-        commands = []
-        for i in range(len(distances)):
+        self.distances, self.directions = self.get_distance_direction(num)
+        for i in range(len(self.distances)):
             # find angle to turn
-            self.new_direction = directions[i]
+            self.new_direction = self.directions[i]
             angle_old_cos = arccos(self.old_direction[0])
             angle_old_sin = arcsin(self.old_direction[1])
             if angle_old_sin < 0:  # express angle always in 0 to 2pi
@@ -86,55 +84,34 @@ class navstar:
             #print('theta',theta)
 
             # calculate linear velocity
-            self.linear_velocity = distances[i] / self.time_step * self.converting_factor
+            self.linear_velocity = self.distances[i] / self.time_step * self.converting_factor
 
             # calculate angular velocity
             self.angular_velocity = theta / self.time_step
 
             #print('lin velocity', self.linear_velocity)
             #print('ang velocity', self.angular_velocity)
-
+            
+            self.vel_msg.linear.x = self.linear_velocity
+            self.vel_msg.linear.y = 0
+            self.vel_msg.linear.z = 0
+            self.vel_msg.angular.x = 0
+            self.vel_msg.angular.y = 0
+            self.vel_msg.angular.z = self.angular_velocity
+            self.pub.publish(self.vel_msg)
             self.commands = [self.linear_velocity, self.angular_velocity]
             #print('commands:', commands)
-            '''
-            for i in range(len(self.commands)):
-                print('commands list: ', self.commands)
-                self.lin_vec = Vector3(self.linear_velocity,0,0)
-                self.ang_vec = Vector3(0,0,self.angular_velocity)
-                self.pub.publish(Twist(linear=self.linear_velocity, angular=self.angular_velocity))
-            '''
-
-    def run_robot(self):
-        # Add to vectors
-        for i in range(len(self.commands)):
-            #print('comms list:', self.commands[i])
-            self.linear_vector = Vector3(self.linear_velocity,0,0)
-            self.angular_vector = Vector3(0,0,self.angular_velocity)
-
-            print('lin vector',self.linear_vector)
-            print('ang vector', self.angular_vector)
-
-            # Publish
-            self.pub.publish(Twist(linear=self.linear_vector, angular=self.angular_vector))
-            
-            # add to commands
-            self.commands.append(self.angular_velocity)
-            self.commands.append(self.linear_velocity)
-
-            #print('lin_vel:',self.linear_velocity)
-            #print('ang_vel:',self.angular_velocity)
-
-            # update old direction
             self.old_direction = self.new_direction
             self.rate.sleep()
+        else:
+            self.lin_vec = Vector3(0,0,0)
+            self.ang_vec = Vector3(0,0,0)
+            self.pub.publish(Twist(linear=self.lin_vec, angular=self.ang_vec))
+            rospy.signal_shutdown("reached target destination...shutting down now") 
 
-            #return commands
-            #print('commands:', self.commands)
-   
 if __name__ == '__main__':
     while not rospy.is_shutdown():
         nav = astar()
-        coordinates = nav.actualAStar((585,190),(665,225),"../maps/ac109_goodmap.png") #Coords for round trashcan to corner of box
+        coordinates = nav.run_astar((300,290),(575,215),"../maps/ac109_goodmap.png") #Coords for round trashcan to corner of box
         robot = navstar(coordinates, 1)
         robot.get_velocity_commands(10)
-        robot.run_robot()
